525101d208e73e35968ae7887033fc4885ca90f4
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-16 17:56:17 -0400

    added spotify cards

--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Typography } from '@mui/material'
-
+import { Spotify } from 'react-spotify-embed'
 function Home({theme}) {
   return (
-    <Typography sx={{
-      color: theme.palette.background.default
-  
-          }}>Main section</Typography>
+    <>
+      <Typography sx={{
+        color: theme.palette.background.default
+            }}>Main section</Typography>
+            <Spotify  link="https://open.spotify.com/track/6QS2nWbbANR2EGuhYcjEhu?si=2870d3e7192a497b" />
+            <Spotify link="https://open.spotify.com/album/0uJOBzHP3SAIMfL8Gf5CSv?si=ScqzO4b5Q0qH-gWAoXcooQ" />
+
+
+    </>
   )
 }
 
