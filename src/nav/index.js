37cb4ddd808db35bc165e5da84a93deda48eacdf
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-18 14:23:17 -0400

    refactor Drawer List;

--- src/nav/index.js
+++ src/nav/index.js
@@ -2,18 +2,10 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import { styled, useTheme } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+
 import { Menu } from '@mui/icons-material';
-import MailIcon from '@mui/icons-material/Mail';
-import HomeIcon from '@mui/icons-material/Home';
 import { Typography, Button } from '@mui/material';
+import DrawerList from './DrawerList';
 
 
 function NavBar({Link}) {
@@ -24,17 +16,6 @@ function NavBar({Link}) {
   };
 
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  }));
 
   const NavContainer = styled('div')(({theme}) => ({
      
@@ -48,34 +29,7 @@ function NavBar({Link}) {
     }));
 
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-      <List>
-        {['Home', 'About', 'Contact'].map((text, index) => {
-          
-          const lowerCase =index === 0 ? "" : text.split().map((letter, idx) => idx === 0 ? letter.toLocaleLowerCase() : letter ).join();
-          console.log(lowerCase);
-          return (
-          <ListItem key={text} disablePadding>
-                <Link to={`/${lowerCase}`}>
-            <ListItemButton>
-              <ListItemIcon>
-                  {text === "Home" ? <HomeIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-                  </Link>
-          </ListItem>
-        )})}
-      </List>
-
-    </Box>
-  );
+  
 
   return (
     <NavContainer sx={{color: theme.palette.background.default,}} > 
@@ -89,7 +43,7 @@ function NavBar({Link}) {
       ))}
       </Box>
       <Drawer open={open} onClose={toggleDrawer(false)} anchor='right'>
-        {DrawerList}
+        <DrawerList toggleDrawer={toggleDrawer} setOpen={setOpen} theme={theme} styled={styled} Link={Link}/>
       </Drawer>
     </NavContainer>
   );
