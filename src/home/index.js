df2c7e0767bf3ea52704d735a31934cca58f95f5
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-17 20:04:42 -0400

    styling box component

--- src/home/index.js
+++ src/home/index.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { Typography, styled } from '@mui/material'
-import { Spotify } from 'react-spotify-embed'
-function Home({theme}) {
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { Spotify } from 'react-spotify-embed';
 
-  const Container = styled('div')(({theme}) => ({
-    display:'flex',
-    flexDirection: 'column', 
-    justifyContent: 'center',
-    alignItems: 'center'
-  }));
+function Home({ theme }) {
   return (
-    <Container>
-      <Typography sx={{
-        color: theme.palette.background.default
-            }}>Main section</Typography>
-            <Spotify  link="https://open.spotify.com/track/6QS2nWbbANR2EGuhYcjEhu?si=2870d3e7192a497b" />
-            <Spotify link="https://open.spotify.com/album/0uJOBzHP3SAIMfL8Gf5CSv?si=ScqzO4b5Q0qH-gWAoXcooQ" />
-
-
-    </Container>
-  )
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: {xs : 'column',  sm: 'row'},
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh', // Ensures the Box takes the full height of the viewport
+        mx: 'auto',
+      }}
+    >
+      <Typography
+        sx={{
+          color: theme.palette.background.default,
+        }}
+      >
+        Main section
+      </Typography>
+      <Spotify link="https://open.spotify.com/track/6QS2nWbbANR2EGuhYcjEhu?si=2870d3e7192a497b" />
+      <Spotify link="https://open.spotify.com/album/0uJOBzHP3SAIMfL8Gf5CSv?si=ScqzO4b5Q0qH-gWAoXcooQ" />
+    </Box>
+  );
 }
 
-export default Home
+export default Home;
