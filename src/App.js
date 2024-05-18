a1f4032706465e678f62571484ad6e5d4cf7e76a
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-18 14:46:45 -0400

    added background image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import NavBar from './nav';
-import { styled, useTheme } from '@mui/material';
+import { styled, useTheme, Box } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './home'
 import About from './about'
 import Contact from './contact'
-
+import HomeBG from './assets/home_background.jpg'
 export default function App() {
   const theme = useTheme();
   const MainContainer = styled('div')(({theme}) =>( {
     display: 'flex', // Use flexbox
     flexDirection: 'column', // Column layout
     minHeight: '100vh', // Ensure the container fills the viewport height
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: 'none',
+
+    // transform: `rotate(${180})'`
   }));
 
   
@@ -20,14 +22,19 @@ export default function App() {
   return (
     <MainContainer>
       <Router>
-      <NavBar Link={Link} />
+        <Box sx={{    backgroundImage: `url(${HomeBG})`, // Correct syntax for setting background image
+    backgroundSize: 'cover', // Ensure the background image covers the whole container
+    backgroundPosition: 'center', // Center the background image
+  }}>
         
-  
-        <Routes>
-        <Route path="/" element={<Home theme={theme}/>} />
-          <Route path="/about" element={<About theme={theme}/>} />
-          <Route path="/contact" element={<Contact theme={theme}/>} />
-        </Routes>
+        <NavBar Link={Link} />
+        
+          <Routes>
+          <Route path="/" element={<Home theme={theme}/>} />
+            <Route path="/about" element={<About theme={theme}/>} />
+            <Route path="/contact" element={<Contact theme={theme}/>} />
+          </Routes>
+      </Box>
       </Router>
     </MainContainer>
   );
