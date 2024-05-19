647a369901eac4602d3c2c1416a379e9814c89a1
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-19 16:15:51 -0400

    styling the homeage

--- src/home/index.js
+++ src/home/index.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import { Box, Button, Typography } from "@mui/material";
+import headshot1 from "../assets/headshot1.jpg";
 
 function Home({ theme }) {
   return (
     <Box
       sx={{
         display: "flex",
-        flexDirection: { xs: "column", sm: "row" },
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "space-around",
         width: "100%",
-        height: "100vh", // Ensures the Box takes the full height of the viewport
+        height: "85vh", // Ensures the Box takes the full height of the viewport
         mx: "auto",
       }}
     >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: "center",
 
+          justifyContent: "space-between",
+          width: "90%",
+        }}
+      >
+        <Box sx={{ width: 250 }}>
+          <img
+            src={headshot1}
+            style={{ width: "100%" }}
+            alt="home page hero card"
+          ></img>
+        </Box>
+        <Box sx={{ alignSelf: { xs: "auto", md: "end" } }}>
+          <Typography>New Single 'THIRTEEN' out NOW</Typography>
+          <Button>
+            <MusicNoteIcon />
+            Listen
+          </Button>
+        </Box>
+      </Box>
     </Box>
   );
 }
